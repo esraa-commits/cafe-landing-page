e6eec39978be7a2f21bcc98f2a012da8adf469c3
esraa-commits <aassrraa5@gmail.com>
2026-07-25 14:43:51 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,13 @@
+export const menu = [{
+  name : 'coffee',
+  img : 'img/coffee.jpg',
+  price : 3
+}, {
+  name : 'sandwitch',
+  img : 'img/sandwitch.jpg',
+  price : 5
+}, {
+  name : 'waffle',
+  img : 'img/waffle.avif',
+  price : 4
+}];
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import {menu} from '/menu.js';
+
 const homeBtn = document.getElementById("home-page-button");
 const menuBtn = document.getElementById("menu-button");
 const aboutBtn = document.getElementById("about-us-button");
@@ -13,4 +15,26 @@ function showPage(id){
 homeBtn.onclick = () => showPage("home-page");
 menuBtn.onclick = () => showPage("menu-page");
 aboutBtn.onclick = () => showPage("about-page");
+
+
+let html = '';
+menu.forEach((item) => {
+  html += `
+          <div class="menu-item">
+            <div>
+              <img src="img/${item.name}.jpg">
+            </div>
+            <p>
+              ${item.name}
+            </p>
+            <p>
+              ${item.price}$
+            </p>
+          </div>
+  `
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.menu-items-container').innerHTML = html;
+});
 
